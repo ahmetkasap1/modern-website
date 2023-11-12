1d47b595f3a03a87f52800278faff40328b10bd8
ahmetkasap1 <ahmetkasap0@hotmail.com>
2023-11-12 17:24:22 +0300

    start and added navbar

---
+++ app/page.js
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+      <>
+        <div className="w-4/5 mx-auto ">
+          <Navbar></Navbar>
+        </div>
+      </>
+  )
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,38 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+
+
+
+
+const Navbar = () => {
+  return (
+    <>
+      <nav className='flex flex-row items-center justify-evenly  mt-16  '>
+        <div className='basis-1/12 ml-32'>
+            <Image src="/logo.png" alt='logo' width={70} height={70} className=''/> 
+        </div>
+
+        <div className='basis-auto '>
+            <ul className='flex  flex-row gap-6 mr-16'>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>Home</li></Link>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>Lifesycle</li></Link>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>Neouron</li></Link>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>Future</li></Link>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>News</li></Link>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>About Us</li></Link>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>Careers</li></Link>
+                <Link href='/' className='text-white text-lg hover:text-gray-500 font-roboto'><li>Careers</li></Link>
+            </ul>
+        </div>
+        <div className='basis-2/12'>
+            <Link href='/' className='text-black font-roboto text-xl p-3 bg-white hover:bg-black hover:text-white border-2 rounded-2xl'>Contact Us</Link>
+        </div>
+
+      </nav>
+
+    </>
+  )
+}
+
+export default Navbar
